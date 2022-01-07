949ddff30ca6568382e4ca8b5033fdb0663230fe
phollaki <phollakos15@gmail.com>
2022-01-07 17:28:47 +0100

    delete quotes state

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./Header.css";
 
 function Header() {
-  const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState({});
 
   useEffect(() => {
